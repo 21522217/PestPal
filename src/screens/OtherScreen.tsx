7bba01020435ac8f03f14c3f9077cd2b84879728
tonyanhkhoi <tonysquadgamer@gmail.com>
2024-04-20 11:02:25 +0700

    Add Bottom Tab Bar

---
+++ src/screens/OtherScreen.tsx
@@ -0,0 +1,13 @@
+// OtherScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const OtherScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style= {{color:'#000000'}}>Other Screen</Text>
+    </View>
+  );
+};
+
+export default OtherScreen;
